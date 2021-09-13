f84d08d5d17ec315cae70e9425d37cd5bf815b91
Rouzbeh1797 <rouzbeh1797@gmail.com>
2021-09-12 23:08:00 -0600

    corrected spelling

--- src/App.js
+++ src/App.js
@@ -12,41 +12,14 @@ import sample from "./image/Sample.jpg";
 function App() {
   return (
     <>
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
-              <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-            <Nav>
-              <Nav.Link href="#deets">More deets</Nav.Link>
-              <Nav.Link eventKey={2} href="#memes">
-                Dank memes
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
       <header>
-        <div className="header-text">
-          Hello and welcome to{" "}
-          <span className="different_color"> R.Ghazi </span> Protfolio webpage
+        <div className=" container ">
+          <div className="header-text container ">
+            <p>
+              Hello and welcome to{" "}
+              <span className="different_color">R.Ghazi</span> porfolio webpage
+            </p>
+          </div>
         </div>
       </header>
 
@@ -57,7 +30,7 @@ function App() {
           </div>
           <div className="text">
             <div className="title">About me</div>I am a full stack software
-            deveoper. I'm passan ation about programming and learning new
+            developer. I'm passionate about programming and learning new
             technologies. Other than programming my main hobby is hiking.
           </div>
         </div>
@@ -73,18 +46,35 @@ function App() {
             <div className="title">Skills</div>
             <Container fluid>
               <Row className="height1">
-                <Col md={4}>HTML</Col>
-                <Col md={4}>CSS</Col>
-                <Col md={4}>Git & Github</Col>
+                <Col md={4}>
+                  {" "}
+                  <i class="fab fa-html5"></i> HTML
+                </Col>
+                <Col md={4}>
+                  {" "}
+                  <i class="fab fa-css3-alt"></i> CSS
+                </Col>
+                <Col md={4}>
+                  <i class="fab fa-github"></i> Git & GitHub
+                </Col>
               </Row>
               <Row className="height1">
-                <Col md={4}>Python</Col>
-                <Col md={4}>Djnago</Col>
-                <Col md={4}>React</Col>
+                <Col md={4}>
+                  {" "}
+                  <i class="fab fa-python"></i> Python
+                </Col>
+                <Col md={4}>Django </Col>
+                <Col md={4}>
+                  {" "}
+                  <i class="fab fa-react"></i> React
+                </Col>
               </Row>
               <Row className="height1">
                 <Col md={4}>TypeScript</Col>
-                <Col md={4}>Node.js</Col>
+                <Col md={4}>
+                  {" "}
+                  <i class="fab fa-node"></i> Node.js
+                </Col>
                 <Col md={4}>REST API</Col>
               </Row>
             </Container>
@@ -108,22 +98,30 @@ function App() {
         <div className="text   samle_project">
           <Container className=" height100  ">
             <Row className="height100  width100 ">
-              <Col md={6} className="r">
-                <img src={sample} alt="" />
+              <Col md={6} className="">
+                <a
+                  href="https://www.onlineshopsample.ca"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  <img src={sample} alt="" />
+                </a>
               </Col>
-              <Col md={6} className="r">
-                <h5>Ecommerce Website</h5>
-                Technologies:
+              <Col md={6} className="">
+                <h5>E-commerce Website</h5>
+                <h6>Technologies:</h6>
                 <Row className="justify-content-md-center">React</Row>
                 <Row className="justify-content-md-center">Node.js</Row>
+                <Row className="justify-content-md-center">MongoDb</Row>
                 <Row>
+                  <div className="spacer5rem"></div>
                   <Col>
                     <a
                       href="https://www.onlineshopsample.ca/"
                       target="_blank"
                       rel="noreferrer noopener"
                     >
-                      <Button> Website</Button>
+                      <Button variant="warning"> Website</Button>
                     </a>
                   </Col>
                   <Col>
@@ -132,7 +130,56 @@ function App() {
                       target="_blank"
                       rel="noreferrer noopener"
                     >
-                      <Button> GitHub Link</Button>
+                      <Button variant="warning"> GitHub Link</Button>
+                    </a>
+                  </Col>
+                </Row>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      </section>
+      <section className="container width ">
+        <div className="text   samle_project">
+          <Container className=" height100  ">
+            <Row className="height100  width100 ">
+              <Col md={6} className="">
+                <a
+                  href="https://www.onlineshopsample.ca"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  <img src={sample} alt="" />
+                </a>
+              </Col>
+              <Col md={6} className="">
+                <h5>E-commerce Website</h5>
+                <h6>
+                  Same frontend as above project but different backend and
+                  database
+                </h6>
+                <h6>Technologies:</h6>
+                <Row className="justify-content-md-center">React</Row>
+                <Row className="justify-content-md-center">Django (Python)</Row>
+                <Row className="justify-content-md-center">PostGreSQL</Row>
+                <Row>
+                  <div className="spacer5rem"></div>
+                  <Col>
+                    <a
+                      href="https://www.onlineshopsample.ca/"
+                      target="_blank"
+                      rel="noreferrer noopener"
+                    >
+                      <Button variant="warning"> Website</Button>
+                    </a>
+                  </Col>
+                  <Col>
+                    <a
+                      href="https://github.com/Ross-Ghazi/eCommerce-React-Django"
+                      target="_blank"
+                      rel="noreferrer noopener"
+                    >
+                      <Button variant="warning"> GitHub Link</Button>
                     </a>
                   </Col>
                 </Row>
